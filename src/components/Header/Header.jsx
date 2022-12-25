9dabaf7b13c6f7e9339718e4a0137056a04105b7
Tomás <ttomycastro@gmail.com>
2022-12-25 14:35:01 -0300

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+import { NavLink } from 'react-router-dom'
+import './header.css'
+
+import {motion} from 'framer-motion'
+
+import logo from '../../assets/images/eco-logo.png'
+import userIcon from '../../assets/images/user-icon.png'
+
+import { Container, Row } from 'reactstrap'
+
+const nav__links = [
+  {
+    path: 'home',
+    display: 'Inicio'
+  },
+  {
+    path: 'shop',
+    display: 'Shopping'
+  },
+  {
+    path: 'cart',
+    display: 'Carro'
+  },
+]
+
+const Header = () => {
+  return <header className='header'>
+    <Container>
+      <Row>
+        <div className='nav__wrapper'>
+          <div className='logo'>
+            <img src={logo} alt='logo '/>
+            <div>
+              <h1>NeuSmart</h1>
+              <p>Creado en 2021</p>
+            </div>
+          </div>
+
+          <div className='navigation'>
+            <ul className='menu'>
+              {nav__links.map((item, index) => (
+                <li className='nav__item' key={index}>
+                <NavLink to={item.path} className={(navClass) => navClass.isActive ? 'nav__active' : ''}>{item.display}</NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='nav__icons'>
+
+
+          <span className='fav__icon'>
+            <i class="ri-service-line"></i>
+            <span className='badge'>1</span>
+          </span>
+            <span className='cart__icon'>
+              <i class="ri-shopping-basket-line"></i>
+              <span className='badge'>1</span>
+            </span>
+
+            <span>
+              <motion.img whileTap={{scale: 1.2}} src={userIcon} alt=''/>
+            </span>
+          </div>
+
+          <div className='mobile__menu'>
+            <span><i class="ri-menu-line"></i></span>
+          </div>
+
+
+        </div>
+      </Row>
+    </Container>
+  </header>
+}
+
+export default Header
